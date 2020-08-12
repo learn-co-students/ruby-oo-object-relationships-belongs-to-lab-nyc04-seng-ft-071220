aad547058e159a3999e255a5a059e2ae5b17e697
hortencia c <hortenciaecisneros@gmail.com>
2020-08-12 17:27:29 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+class Artist
+ attr_accessor :name
+  
+end 
+# drake = Artist.new
+# drake.name = "drake"
+# p drake.name
+
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song 
+  attr_accessor :title,:artist
+  
+  
+ 
+  
+end
+# song = Song.new("hello")
+# song.artist = drake
+# p song.artist.name
+# a title blongs to songclass, that has been written by an "artist" 
